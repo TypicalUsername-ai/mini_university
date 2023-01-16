55778da98403f6add1f2379c3102c4794741c2e0
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-01-16 14:16:08 +0100

    fixed package json and backend in frontend issues

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,25 +1,17 @@
 import logo from '../../logo.svg';
 import './style.css';
 import { Link } from 'react-router-dom';
-import mysql from "mysql2";
 import { useEffect } from 'react';
 
 
 export function HomePage() {
   useEffect( () => {
-    console.log("trying to establish sql connection");
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'worker',
-      password: 'worker_password',
-      database: 'mini_university'
-    });
   })
   return (
     <div className="App">
       <header className='App-header'> <p>Mateusz's Databases 1 final project</p> </header>
       <div className='App-main'>
-
+        
       </div>
     </div>
   );
