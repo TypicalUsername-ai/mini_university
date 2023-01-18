c7190b316f9b436bf4cd2bd7d90ebc7440a236ff
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-01-18 15:28:43 +0100

    add student  done

--- frontend/src/pages/AddStudent/index.tsx
+++ frontend/src/pages/AddStudent/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface student {
@@ -7,21 +8,67 @@ interface student {
     id_positions: number
 }
 
+interface position {
+    id_positions: number,
+    position: String,
+}
+
 async function getStudents(): Promise<student[]> {
     const res: student[] = await (await fetch("http://localhost:3002/api/participants")).json();
     return res
 }
 
+async function getPositions(): Promise<position[]> {
+    const res: position[] = await (await fetch("http://localhost:3002/api/positions")).json();
+    return res
+}
+
 export function AddStudent() {
 
+    const[student, setStudent] = useState<student>({
+        id_participants: -1,
+        family_name: "",
+        initial_name: "",
+        id_positions: 0
+    });
+    const[students, setStudents] = useState<student[]>([]);
+    const[positions, setPositions] = useState<position[]>([]);
+
+    useEffect(() => {
+        getStudents().then(a => setStudents(a));
+        getPositions().then(a => setPositions(a));
+    }, [])
+
     async function postStudent(){
-        
+        const res: student[] = await (await fetch(`http://localhost:3002/api/participants?id=${students[students.length -1].id_participants + 1}&name=${student.initial_name}&lname=${student.family_name}&position=${student.id_positions}`, {method: "post"})).json();
+        return res
     }
 
     return(
         <div className="App">
             <Link to="/">go Home</Link>
-            "this is the add page :)"
+            <div>
+                <input placeholder="first name" onChange={(e) => {
+                    var val = student;
+                    val.initial_name = e.target.value
+                    setStudent(val);
+                }}/>
+                <input placeholder="last name" onChange={(e) => {
+                    var val = student;
+                    val.family_name = e.target.value
+                    setStudent(val);
+                }}/>
+                <select name="positions" onChange={(e) => {
+                    var val = student;
+                    val.id_positions = Number.parseInt(e.target.value);
+                    setStudent(val);
+                }}>
+                    {positions.map(
+                        p => <option value={p.id_positions}>{p.position}</option>
+                    )}
+                </select>
+                <button onClick={() => postStudent()}> add </button>
+            </div>
         </div>
     );
 }
