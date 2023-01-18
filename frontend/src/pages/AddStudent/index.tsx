24687960131039a4e0f067626c34f47f740d3355
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-01-18 14:48:54 +0100

    student add WIP

--- frontend/src/pages/AddStudent/index.tsx
+++ frontend/src/pages/AddStudent/index.tsx
@@ -1,6 +1,23 @@
 import { Link } from "react-router-dom";
 
+interface student {
+    id_participants: number,
+    family_name: String,
+    initial_name: String,
+    id_positions: number
+}
+
+async function getStudents(): Promise<student[]> {
+    const res: student[] = await (await fetch("http://localhost:3002/api/participants")).json();
+    return res
+}
+
 export function AddStudent() {
+
+    async function postStudent(){
+        
+    }
+
     return(
         <div className="App">
             <Link to="/">go Home</Link>
